b379576cf69783b971f05a0c2b580e08207944fe
gopal gupta <gopalgupta2804@gmail.com>
2023-04-06 12:27:47 +0530

    update blog and delete blog controller done (backend)

--- server/controllers/blogController.js
+++ server/controllers/blogController.js
@@ -31,3 +31,35 @@ const createBlog=async(req,res)=>{
         res.statu(500).send('Server Error');
     }
 }
+
+//updating the blog
+//access-private
+const updateBlog=async(req,res)=>{
+    try{
+        const {title,content}=req.body;
+        const blog=await Blog.findOneAndUpdate({_id:req.params.id,user:req.user.id},{title,content},{new : true});
+        res.json(blog);
+
+    }catch(error){
+        console.error(`ERROR: ${error.message}`.bgRed.underline.bold);
+        res.statu(500).send('Server Error');
+    }
+}
+
+//delete blog
+//access-private
+const deteleBlog=async(req,res)=>{
+    try{
+        res.send('deleting blog');
+    }catch(error){
+        console.error(`ERROR: ${error.message}`.bgRed.underline.bold);
+        res.statu(500).send('Server Error');
+    }
+}
+
+module.exports={
+    deteleBlog,
+    updateBlog,
+    createBlog,
+    getBlog
+}
