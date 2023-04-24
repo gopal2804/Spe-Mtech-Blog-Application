915c1ae2c222f65b1aee2ff3512cc52ed2b197fd
gopal gupta <gopalgupta2804@gmail.com>
2023-04-25 00:28:00 +0530

    logs file updated for docker compose

--- server/utils/logger.js
+++ server/utils/logger.js
@@ -12,7 +12,7 @@ const logger = createLogger({
   transports: [
     new transports.Console(),
     new winston.transports.File({
-      filename: "Blog-log.log",
+      filename: "/usr/src/app/logs/Blog-log.log",
       level: "info",
     }),
   ],
