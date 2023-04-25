76401c2c1540b425f2681f31b19c95ad04dc24f5
gopal gupta <gopalgupta2804@gmail.com>
2023-04-25 17:52:14 +0530

    logger code change

--- server/utils/logger.js
+++ server/utils/logger.js
@@ -1,11 +1,21 @@
 const winston = require("winston");
+const fs=require('fs');
+
 const { createLogger, format, transports } = require("winston");
 
 const { combine, timestamp, label, printf } = format;
 
+const logDir='/logs';
+
+if (!fs.existsSync(logDir)) {
+  fs.mkdirSync(logDir);
+}
+
 const myFormat = printf(({ level, message, label, timestamp }) => {
   return `${timestamp} ${label} ${level}: ${message}`;
 });
+
+
 
 const logger = createLogger({
   format: combine(label({ label: "" }), timestamp(), myFormat),
@@ -13,7 +23,7 @@ const logger = createLogger({
     new transports.Console(),
     new winston.transports.File({
       // filename: "/usr/src/app/logs/Blog-log.log",
-      filename: "Blog-log.log",
+      filename: `${logDir}/Blog-log.log`,
       level: "info",
     }),
   ],
