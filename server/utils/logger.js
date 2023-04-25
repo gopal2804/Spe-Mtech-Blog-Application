e96086622ab3fecbf0760451f9d1f8c1ee63eeb0
gopal gupta <gopalgupta2804@gmail.com>
2023-04-25 18:51:46 +0530

    docker compose log file

--- server/utils/logger.js
+++ server/utils/logger.js
@@ -1,15 +1,15 @@
 const winston = require("winston");
-const fs=require('fs');
+// const fs=require('fs');
 
 const { createLogger, format, transports } = require("winston");
 
 const { combine, timestamp, label, printf } = format;
 
-const logDir='/home/gopal/Desktop/Semester_2/SPE/MajorProject/Spe-Mtech-Blog-Application/logs';
+// const logDir='/home/gopal/Desktop/Semester_2/SPE/MajorProject/Spe-Mtech-Blog-Application/logs';
 
-if (!fs.existsSync(logDir)) {
-  fs.mkdirSync(logDir);
-}
+// if (!fs.existsSync(logDir)) {
+//   fs.mkdirSync(logDir);
+// }
 
 const myFormat = printf(({ level, message, label, timestamp }) => {
   return `${timestamp} ${label} ${level}: ${message}`;
@@ -22,8 +22,8 @@ const logger = createLogger({
   transports: [
     new transports.Console(),
     new winston.transports.File({
-      // filename: "/usr/src/app/logs/Blog-log.log",
-      filename: `${logDir}/Blog-log.log`,
+      filename: "/usr/src/app/logs/Blog-log.log",
+      // filename: `${logDir}/Blog-log.log`,
       level: "info",
     }),
   ],
